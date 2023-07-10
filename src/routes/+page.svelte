eae98fae4700e1810261ea293e1f1bd8e24b7fdf
Michael Warner <michaelwarner@Michaels-MacBook-Pro-2.local>
2023-07-10 10:47:37 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+  const test = /* tw */ 'bg-red-50';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
